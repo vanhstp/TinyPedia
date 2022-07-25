c1d6dec1eb68ab95e21556c0fa184a9b435510d0
vanhstp MBP <vanhstp@qq.com>
2022-07-25 20:53:54 +0800

    1

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+export default {
+    title: '小百科 (TinyPedia)',
+    description: '专注于一个人（或一些人）的小百科',
+    base: '/TinyPedia/',
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        siteTitle: '小百科 (TinyPedia)',
+        logo: 'https://raw.githubusercontent.com/vanhstp/KunPedia/master/docs/.vitepress/public/KunPedia-02.png',
+        footer: {
+            copyright: 'TinyPedia by vanhstp ｜ Powered by VitePress'
+        },
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '坤坤小百科', link: 'https://vanhstp.github.io/KunPedia/' },
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vanhstp/TinyPedia' },
+        ],
+    },
+}
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+layout: home
+
+hero:
+    name: 小百科 (TinyPedia)
+    text: 专注于一个人（或一些人）的小百科
+    alt: 小百科 (TinyPedia)
+
+features:
+- icon: ✅
+  title: 已完成
+  details: 无
+- icon: 🔍
+  title: 开放预览中
+  details: 坤坤小百科（v0.0.2）
+- icon: 🗓
+  title: 计划中
+  details: 东北小百科、丁真小百科
+- icon: ⌛️
+  title: 维护中
+  details: 无
+
+---
